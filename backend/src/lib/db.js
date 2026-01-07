554c2b4fdab0abc1307e5af2935ea0baec8af677
Vishal verma <vishal.verma3441@gmail.com>
2026-01-07 14:09:31 +0530

    Update db.js with MongoDB connection fix

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,11 +4,15 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+    const mongoUri = process.env.MONGO_URI;   // 👈 SAME NAME AS RENDER
+    if(!mongoUri){
+      throw new Error("MONGO_URI not found in environment variables");
+    }
+
     const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Error in connecting to MongoDB", error);
-    process.exit(1); // 1 means failure
+    process.exit(1);
   }
 };
