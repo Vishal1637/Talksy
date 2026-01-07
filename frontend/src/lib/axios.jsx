876be102be340bc98b8f296062525c7d4738a193
Vishal verma <vishal.verma3441@gmail.com>
2026-01-07 17:18:07 +0530

    Thiis is talksy

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.MODE === "development" ? "https://talksyapp.onrender.com" : "https://backend-ashy-one-63.vercel.app/api");
-
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.MODE === "development" ? "https://talksyapp.onrender.com" : "https://backend-ashy-one-63.vercel.app/api");
+const BASE_URL = "https://talksyapp.onrender.com";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
